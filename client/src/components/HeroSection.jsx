4d8fc4a2faca889578bb40463b7e3918bf2d4017
Prachi <prachimohantyriva2002@gmail.com>
2024-10-05 11:39:08 +0530

    third commit

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+
+
+function HeroSection() {
+  return (
+    <section className="hero_section">
+      <div className="video-div">
+        <video src="/video.mp4" autoPlay muted loop></video>
+      </div>
+      <div className="details">
+        <div className="content">
+            <h1>Welcome to Coding School</h1>
+            <h3>Learn and Implement</h3>
+            <p></p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
